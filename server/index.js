7dd3319a7b066d68fdad2378761cd02eb32efd72
pikmi-vika <vkrupko483@gmail.com>
2026-06-21 02:04:17 +0200

    fix server port

--- server/index.js
+++ server/index.js
@@ -585,6 +585,8 @@ app.get("/api/exercises/:id", (req, res) => {
   res.json(exercise);
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
