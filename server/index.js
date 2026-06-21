b9636b23ff5c39f41893d96fa5cfad4ac2b93f29
pikmi-vika <vkrupko483@gmail.com>
2026-06-21 21:35:59 +0200

    fix login labels and safe email

--- server/index.js
+++ server/index.js
@@ -45,13 +45,14 @@ const sendEmail = async ({ to, subject, html }) => {
   }
 };
 
+
   await transporter.sendMail({
     from: `"Stretching Krupko" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
   });
-};
+
 
 const exercises = [
   {
