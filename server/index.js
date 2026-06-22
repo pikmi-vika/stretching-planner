1b487ff982745b20749e369d782ee7ae20d872e1
pikmi-vika <vkrupko483@gmail.com>
2026-06-22 02:50:59 +0200

    fix render port binding

--- server/index.js
+++ server/index.js
@@ -757,6 +757,6 @@ app.get("/api/exercises/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
